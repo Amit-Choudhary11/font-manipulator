c458c07f756f71262ad333917f4bc582d62061eb
Amit-Choudhary11 <74827051+Amit-Choudhary11@users.noreply.github.com>
2021-11-06 21:13:12 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -6,9 +6,30 @@ function preload(){
 function setup(){
 canvas= createCanvas(500,500);
 canvas.position(800,160);
+video=createCapture(VIDEO);
+video.size(500,500);
+video.position(200,160);
+
+
+poseNet=ml5.poseNet(video,modelloaded);
+
+poseNet.on("pose", gotResults);
 }
 
 
 function draw(){
-
+background("#73FDC5")
 }
+
+function modelloaded(){
+    console.log("model loaded");
+}
+
+function gotResults(results){
+    if(results.length > 0){
+        console.log(results);
+    }
+}
+
+
+
